8b1ef69b80ceb4d85776da1d16ec79d2c9ffbcea
Oleksandr Kosteniuk <Oleksandr.Kosteniuk@epam.com>
2021-06-05 20:46:06 +0300

    JUnit Framework Task 1

--- src/test/java/bookrepository/BookRepositoryTest.java
+++ src/test/java/bookrepository/BookRepositoryTest.java
@@ -29,7 +29,7 @@ public class BookRepositoryTest {
 
         Assert.assertTrue( "Logo is absent", logo.isDisplayed());
         
-        WebElement signInRegister = waitForElementLocatedBy(driver, By.xpath("/html/body/div[4]/div[1]/div/ul[2]/li[3]/a"));
+        WebElement signInRegister = waitForElementLocatedBy(driver, By.xpath("(//*[@href='/account/login/to/account'])[1]"));
 
         Assert.assertTrue("Sign in/Register element is absent", signInRegister.isDisplayed());
         
@@ -43,28 +43,77 @@ public class BookRepositoryTest {
 
         
         searchInputField.sendKeys("The Hidden Child");
-        WebElement searchBtn = driver.findElement(By.xpath("//*[@aria-label='Search']"));
+        WebElement searchBtn = waitForElementLocatedBy(driver,By.xpath("//*[@aria-label='Search']"));
         searchBtn.click();
 
         List<WebElement> searchResults = driver.findElements(By.xpath("//*[@class='book-item']"));
 
         Assert.assertTrue("Search results are empty!",searchResults.size()>0);
 
-        List<WebElement> addToBasketBtn = driver.findElements(By.xpath("//*[contains(text(), 'Add to basket')]"));
+        WebElement productTitle = waitForElementLocatedBy(driver,By.xpath("(//*/h3[@class='title'])[1]"));
+
+        Assert.assertTrue("Product is not found", productTitle.isDisplayed());
         
-        addToBasketBtn.get(0).click();
+        productTitle.click();
+        
+        WebElement addToBasketButton = waitForElementLocatedBy(driver,By.xpath("//*[@class='btn btn-primary add-to-basket']"));
+        
+        addToBasketButton.click();
+        
+        WebElement basketCheckoutButton = waitForElementLocatedBy(driver,By.xpath("//a[contains(text(),'Basket / Checkout')]"));
+        
+        basketCheckoutButton.click();
+                
+        WebElement productLineItemPrice = waitForElementLocatedBy(driver,By.xpath("//*[@class='item-total']"));
+                
+        WebElement orderTotalAmount = driver.findElement(By.xpath("//*[@class='total']/dd"));
+        
+        Assert.assertEquals("Order total amount is incorrect",Double.parseDouble(productLineItemPrice.getText().replaceAll(" €", "").replaceAll(",","")),Double.parseDouble(orderTotalAmount.getText().replaceAll(" €", "").replaceAll(",","")),0);
+        
+        WebElement checkoutButton = waitForElementLocatedBy(driver,By.xpath("(//*[@class='checkout-btn btn'])[1]"));
+        
+        checkoutButton.click();
+        
+        
+        //CO - Delivery Address
+        
+        WebElement emailInputField = waitForElementLocatedBy(driver,By.xpath("//*[@name='emailAddress']"));
+
+        emailInputField.sendKeys("test@user.com");
+
+        WebElement fullName = waitForElementLocatedBy(driver,By.xpath("//*[@name='delivery-fullName']"));
+
+        fullName.sendKeys("Oleksandr Kosteniuk");
+
+        WebElement enterAddressManuallyButton = waitForElementLocatedBy(driver,By.xpath("//*[@id='manualEntryDeliveryAddress']"));
+        
+        enterAddressManuallyButton.click();
+
+        WebElement addressLine1 = waitForElementLocatedBy(driver,By.xpath("//*[@name='delivery-addressLine1']"));
+        addressLine1.sendKeys("Khreschatyk");
+
+        WebElement town = waitForElementLocatedBy(driver,By.xpath("//*[@name='delivery-city']"));
+        town.sendKeys("KYIV");
+                
+        WebElement postcodeZIP = waitForElementLocatedBy(driver,By.xpath("//*[@name='delivery-postCode']"));
+        postcodeZIP.sendKeys("04210");
+        
+        WebElement continueToPaymentButton = waitForElementLocatedBy(driver,By.xpath("//*[@id='hasSubmittedSameAddresses']"));
+        continueToPaymentButton.click();
+
+        WebElement productLineItemInTheSummary = waitForElementLocatedBy(driver,By.xpath("//*[@class='item-price text-right']"));
+        WebElement subtotalInTheSummary = waitForElementLocatedBy(driver,By.xpath("(//*[@class='text-right'])[3]"));
+        WebElement vatInTheSummary = waitForElementLocatedBy(driver,By.xpath("(//*[@class='text-right total-tax'])[2]"));
+        WebElement totalInTheSummary = waitForElementLocatedBy(driver,By.xpath("(//*[@class='text-right total-price'])[2]"));
+        
+        Assert.assertEquals("Order total amount is incorrect",Double.parseDouble(subtotalInTheSummary.getText().replaceAll(" €", "").replaceAll(",",""))+Double.parseDouble(vatInTheSummary.getText().replaceAll(" €", "").replaceAll(",","")),Double.parseDouble(totalInTheSummary.getText().replaceAll(" €", "").replaceAll(",","")),0);
+        
         
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        
         }
-
-        WebElement addToBasketConfirmationPopup = waitForElementLocatedBy(driver, By.xpath("//*[@class='modal-content']"));
-
-        Assert.assertTrue("Item is not added to Cart", addToBasketConfirmationPopup.isDisplayed());
-        
 
         driver.quit();
     }
